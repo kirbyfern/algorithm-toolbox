a09420694a2656fd844e6e4f309d7d0e93413860
kirbygit <superkirbygit@gmail.com>
2020-03-31 00:12:19 -0700

    Initialize repo

---
+++ week1_solution/1_sum_of_two_digits/APlusB.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int sum_of_two_digits(int first_digit, int second_digit)
+{
+    return first_digit + second_digit;
+}
+
+int main()
+{
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+
+    std::cin >> a;
+    std::cin >> b;
+    sum = a + b;
+    std::cout << sum << std::endl;
+    return 0;
+}
