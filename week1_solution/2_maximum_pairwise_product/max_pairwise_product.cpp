53d0fdc3a6f9e52f054d622b5c86780c2bc50a00
kirbygit <superkirbygit@gmail.com>
2020-04-04 22:51:52 -0700

    Second modification for solution for max_pair_product.(Slow solution - Stress Test)

--- week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+long long MaxPairwiseProduct(const std::vector<int> &numbers)
+{
+    long long result = 0;
+    int n = numbers.size();
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            // cast long long data type for bigger memory
+            if (((long long)numbers[i]) * numbers[j] > result)
+            {
+                result = ((long long)numbers[i]) * numbers[j];
+            }
+        }
+    }
+    return result;
+}
 
 int main()
 {
-    std::cout << "test" << std::endl;
+    int n;
+    std::cin >> n;
+    std::vector<int> numbers(n);
 
+    for (int i = 0; i < n; ++i)
+    {
+        std::cin >> numbers[i];
+    }
+
+    long long result = MaxPairwiseProduct(numbers);
+    std::cout << result << "\n";
     return 0;
 }
