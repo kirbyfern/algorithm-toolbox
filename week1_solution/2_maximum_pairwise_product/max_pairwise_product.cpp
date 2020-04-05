a56b5648a89f143f33f023813da2738649d11091
kirbygit <superkirbygit@gmail.com>
2020-04-04 23:10:39 -0700

    Modified the function for better execution time req.

--- week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -2,37 +2,45 @@
 #include <vector>
 #include <algorithm>
 
-long long MaxPairwiseProduct(const std::vector<int> &numbers)
+long long MaxPairwiseProductFast(const std::vector<int> &numbers)
 {
-    long long result = 0;
     int n = numbers.size();
 
+    // find the first maximum element and store its index in variable max_index1
+    int max_index1 = -1;
     for (int i = 0; i < n; ++i)
     {
-        for (int j = i + 1; j < n; ++j)
+        if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
         {
-            // cast long long data type for bigger memory
-            if (((long long)numbers[i]) * numbers[j] > result)
-            {
-                result = ((long long)numbers[i]) * numbers[j];
-            }
+            max_index1 = i;
         }
     }
-    return result;
+
+    // find the second maximum element and store its index in variable max_index2
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j)
+    {
+        if ((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+        {
+            max_index2 = j;
+        }
+    }
+
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
 }
 
 int main()
 {
-    int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
+    // int n;
+    // std::cin >> n;
+    // std::vector<int> numbers(n);
 
-    for (int i = 0; i < n; ++i)
-    {
-        std::cin >> numbers[i];
-    }
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     std::cin >> numbers[i];
+    // }
 
-    long long result = MaxPairwiseProduct(numbers);
+    long long result = MaxPairwiseProductFast(std::vector<int>(100000, 0));
     std::cout << result << "\n";
     return 0;
 }
