bb37d2e7b3dbff66103c45c1c0dc4b82d68814df
kirbygit <superkirbygit@gmail.com>
2020-04-05 20:02:58 -0700

    Parsed the slow function in main rather than the fast function

--- week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -3,50 +3,42 @@
 #include <algorithm>
 #include <cstdlib>
 
-long long MaxPairwiseProduct(const std::vector<int> &numbers)
+using namespace std;
+
+long long MaxPairwiseProduct(const vector<int> &numbers)
 {
     long long result = 0;
     int n = numbers.size();
 
-    for (int i = 0; i < n; ++i)
-    {
-        for (int j = i + 1; j < n; ++j)
-        {
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
             // cast long long data type for bigger memory
-            if (((long long)numbers[i]) * numbers[j] > result)
-            {
-                result = ((long long)numbers[i]) * numbers[j];
+            if (((long long)numbers[i]) * numbers[j] > result) {
+                result = ((long long)(numbers[i])) * numbers[j];
             }
         }
     }
     return result;
 }
 
-long long MaxPairwiseProductFast(const std::vector<int> &numbers)
+long long MaxPairwiseProductFast(const vector<int> &numbers)
 {
     int n = numbers.size();
 
     // find the first maximum element and store its index in variable max_index1
     int max_index1 = -1;
     for (int i = 0; i < n; ++i)
-    {
         if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
-        {
             max_index1 = i;
-        }
-    }
+
 
     // find the second maximum element and store its index in variable max_index2
     int max_index2 = -1;
     for (int j = 0; j < n; ++j)
-    {
         if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
-        {
             max_index2 = j;
-        }
-    }
 
-    //-- Debug line
+     //-- Debug
     // std::cout << max_index1 << ' ' << max_index2 << "\n";
 
     return ((long long)(numbers[max_index1])) * numbers[max_index2];
@@ -55,7 +47,7 @@ long long MaxPairwiseProductFast(const std::vector<int> &numbers)
 int main()
 {
     /*
-    // Implement stress test
+    // Implement Stress Test
     while (true)
     {
         int n = rand() % 1000 + 2;
@@ -84,17 +76,15 @@ int main()
         }
     }
     */
-
     int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
+    cin >> n;
+    vector<int> numbers(n);
 
-    for (int i = 0; i < n; ++i)
-    {
-        std::cin >> numbers[i];
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
     }
 
-    long long result = MaxPairwiseProduct(numbers);
-    std::cout << result << "\n";
+    long long result = MaxPairwiseProductFast(numbers);
+    cout << result << "\n";
     return 0;
 }
