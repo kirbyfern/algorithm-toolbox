1303a10d351ade10d94051436819c76e9444e523
kirbygit <superkirbygit@gmail.com>
2020-04-05 18:33:15 -0700

    Recheck the first function made at hash 53d0fdc

--- week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_solution/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+
+long long MaxPairwiseProduct(const std::vector<int> &numbers)
+{
+    long long result = 0;
+    int n = numbers.size();
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            // cast long long data type for bigger memory
+            if (((long long)numbers[i]) * numbers[j] > result)
+            {
+                result = ((long long)numbers[i]) * numbers[j];
+            }
+        }
+    }
+    return result;
+}
 
 long long MaxPairwiseProductFast(const std::vector<int> &numbers)
 {
