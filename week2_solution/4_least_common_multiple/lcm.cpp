064ad6a036d6ef360848fbd1cca2e2ca2be306c0
kirbygit <superkirbygit@gmail.com>
2020-04-12 17:23:25 -0700

    Completed solution for lcm

--- week2_solution/4_least_common_multiple/lcm.cpp
+++ week2_solution/4_least_common_multiple/lcm.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-#include <algorithm>
+
+typedef long long int lli;
+typedef long int li;
 
 using namespace std;
 
-// TODO:
-// 1. Create a faster solution
-// 2. Solution which is a better computing runtime and lesser memory
-
-//-- Naive Algorithm w/c is much slower--//
-long long lcm_naive(int a, int b) {
+//-- Naive Algorithm w/c is much slower
+lli lcm_naive(int a, int b) {
   for (long l = 1; l <= (long long) a * b; ++l)
     if (l % a == 0 && l % b == 0)
       return l;
@@ -16,9 +14,23 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int gcd_eff(int a, int b) {
+  if (b == 0)
+    return a;
+  // a', the remainder when a is div by b
+  int a_prime = a % b;
+  return gcd_eff(b,a_prime);
+}
+
+
+//-- Efficient Solution
+lli lcm_eff(int a, int b) {
+  return (lli) a * (b / gcd_eff(a, b));
+}
+
 int main() {
   int a, b;
   cin >> a >> b;
-  cout << lcm_naive(a, b) << endl;
+  cout << lcm_eff(a, b) << endl;
   return 0;
 }
