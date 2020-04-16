cddd94604adaed42468b2f0c43da12f72f77ac22
kirbygit <superkirbygit@gmail.com>
2020-04-15 21:14:56 -0700

    Completed efficient solution for huge fibonacci by finding the pisano period

--- week2_solution/5_fibonacci_number_again/fibonacci_huge.cpp
+++ week2_solution/5_fibonacci_number_again/fibonacci_huge.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
-long long get_fibonacci_huge_naive(long long n, long long m) {
+typedef long long  int lli;
+
+using namespace std;
+
+//-- Naive
+lli get_fibonacci_huge_naive(lli n, lli m) {
     if (n <= 1)
         return n;
 
-    long long previous = 0;
-    long long current  = 1;
+    lli previous = 0;
+    lli current  = 1;
 
-    for (long long i = 0; i < n - 1; ++i) {
-        long long tmp_previous = previous;
+    for (lli i = 0; i < n - 1; ++i) {
+        lli tmp_previous = previous;
         previous = current;
         current = tmp_previous + current;
     }
@@ -16,8 +21,42 @@ long long get_fibonacci_huge_naive(long long n, long long m) {
     return current % m;
 }
 
+lli find_pisano_period(lli m) {
+    // previous, current, rest/remainder being initialized
+    lli prev = 0, curr = 1, rest = prev + curr;
+
+    // return the calculation of the period of pisano
+    for (lli i = 0; i < m * m; ++i) {
+        rest = (prev + curr) % m;
+        prev = curr;
+        curr = rest;
+        if (prev == 0 && curr == 1) // The period always starts with 01 and is known as Pisano period. 
+            return i + 1;
+    }
+}
+
+// Efficient Solution
+// Output Format: Output Fn mod m
+lli get_fibonacci_huge_eff(lli n, lli m) {
+
+    // Getting the priod
+    lli pisano_period = n % find_pisano_period(m);
+    // period length of mod n
+    // n %= pisano;
+
+    // previous as a , current as b , rest/remainder as c being initialized as pisano
+    lli a = 0, b = 1, c = pisano_period;
+    n = pisano_period;
+    for (lli i = 1; i < n; ++i) {
+        c = (a + b) % m;
+        a = b;
+        b = c;
+    }
+    return c % m;
+}
+
 int main() {
-    long long n, m;
-    std::cin >> n >> m;
-    std::cout << get_fibonacci_huge_naive(n, m) << '\n';
+    lli n, m;
+    cin >> n >> m;
+    cout << get_fibonacci_huge_eff(n, m) << endl;
 }
