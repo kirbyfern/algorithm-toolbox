3aa901c34eb2fdf5f56e10ebaa4a918d048a235b
kirbygit <superkirbygit@gmail.com>
2020-04-18 21:04:14 -0700

    Needed to fixed method max_dot_product

--- week3_solution/4_maximum_advertisement_revenue/dot_product.cpp
+++ week3_solution/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,58 +1,54 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-#include <random>
 
 
-using std::vector;
-using std::cin;
-using std::cout;
-using std::endl;
+// // where z is the highest value in an index
+// long long get_highest_value(vector<int> z) {
+//   // set z to max
+//   long long max = z[0];
+//   long long index = 0;
+//   for (long long i = 0; i < z.size(); i++)
+//   {
+//     if (max < z[i])
+//     {
+//       max = z[i];
+//       index = i;
+//     }
+//   }
+//   return index;
+// }
 
-// where z is the highest value in an index
-long long get_highest_value(vector<int> z) {
-  // set z to max
-  long long max = z[0];
-  long long index = 0;
-  for (long long i = 0; i < z.size(); i++)
-  {
-    if (max < z[i])
-    {
-      max = z[i];
-      index = i;
-    }
-  }
-  return index;
-}
-
-// FIXME: POINTER ALLOCATION INVALID
-long long max_dot_product(vector<int> a, vector<int> b) {
+// FIXME: 
+long long max_dot_product(std::vector<int> a, std::vector<int> b) {
   // write your code here
+  std::vector<int>::iterator a_max_pos;
+  std::vector<int>::iterator b_max_pos;
   long long result = 0;
   for (long long i = 0; i < a.size(); i++) {
-    long long a_max = get_highest_value(a);
-    long long b_max = get_highest_value(b);
-    result += (a[a_max] * b[b_max]);
-    delete & a[a_max];
-    delete & b[b_max];
+    auto const a_max_pos = std::max_element( a.begin( ), a.end( ) );
+    auto const b_max_pos = std::max_element( b.begin( ), b.end( ) );
+    result += *a_max_pos * *b_max_pos;
+    a.erase( a_max_pos );
+    b.erase( b_max_pos );
   }
   return result;
 }
 
 int main() {
   size_t n;
-  cin >> n;
-  vector<int> a(n), b(n);
+  std::cin >> n;
+  std::vector<int> a(n), b(n);
   for (size_t i = 0; i < n; i++) {
-    cin >> a[i];
+    std::cin >> a[i];
   }
   for (size_t i = 0; i < n; i++) {
-    cin >> b[i];
+    std::cin >> b[i];
   }
-  cout << max_dot_product(a, b) << endl;
+  std::cout << max_dot_product(a, b) << std::endl;
 }
 
-// FIXME: TEST INPUT 2
+// FIXME: 
 // TEST INPUT 1:
 // 1
 // 23
@@ -64,4 +60,4 @@ int main() {
 // 3
 // 1 3 -5
 // -2 4 1
-// OUTPUT: INVALID POINTER
+// 13 - Should be 23 - Fail
