ff38338dd2f06b55418d5e59a9ba887bf9928beb
kirbygit <superkirbygit@gmail.com>
2020-04-19 20:13:26 -0700

    Simplified solution

--- week3_solution/4_maximum_advertisement_revenue/dot_product.cpp
+++ week3_solution/4_maximum_advertisement_revenue/dot_product.cpp
@@ -1,20 +1,19 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <random>
+
+typedef long long int lli;
 
 
-// FIXME: 
-long long max_dot_product(std::vector<int> a, std::vector<int> b) {
-  // write your code here
-  std::vector<int>::iterator a_max_pos;
-  std::vector<int>::iterator b_max_pos;
-  long long result = 0;
-  for (long long i = 0; i < a.size(); i++) {
-    auto const a_max_pos = std::max_element( a.begin( ), a.end( ) );
-    auto const b_max_pos = std::max_element( b.begin( ), b.end( ) );
-    result += *a_max_pos * *b_max_pos;
-    a.erase( a_max_pos );
-    b.erase( b_max_pos );
+lli max_dot_product(std::vector<int> a, std::vector<int> b) {
+  lli result = 0;
+  std::sort(a.begin(), a.end(), std::greater<>());
+  std::sort(b.begin(), b.end(), std::greater<>());
+  for (lli i = 0; i < a.size(); i++)
+  {
+    // typecast
+    result += ((lli) a[i]) * b[i];
   }
   return result;
 }
@@ -31,17 +30,3 @@ int main() {
   }
   std::cout << max_dot_product(a, b) << std::endl;
 }
-
-// FIXME: 
-// TEST INPUT 1:
-// 1
-// 23
-// 39
-// 897
-// OUTPUT: 897 - Passed
-
-// TEST INPUT 2:
-// 3
-// 1 3 -5
-// -2 4 1
-// 13 - Should be 23 - Fail
