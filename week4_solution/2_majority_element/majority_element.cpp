568bf75c9591c675866a112f2f05b321c62b1532
kirbygit <superkirbygit@gmail.com>
2020-04-25 18:36:54 -0700

    Completed Solution but failed on a certain case

--- week4_solution/2_majority_element/majority_element.cpp
+++ week4_solution/2_majority_element/majority_element.cpp
@@ -4,19 +4,28 @@
 
 using std::vector;
 
-int get_majority_element(vector<int> &a, int left, int right) {
+int get_majority_element(vector<int> &z, int left, int right, int &n) {
   if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //code here
+  if (left + 1 == right) return z[left];
+  std::sort(z.begin(), z.end());
+  // for (auto a : z) std::cout << a << " ";
+  int current_element = z[0], counter = 0;
+  for (int i = 0; i < n; i++)
+  {
+    if (z[i] == current_element) counter++;
+  }
+  if (counter > n/2) return current_element;
+
   return -1;
 }
 
 int main() {
   int n;
   std::cin >> n;
-  vector<int> a(n);
-  for (size_t i = 0; i < a.size(); ++i) {
-    std::cin >> a[i];
+  vector<int> z(n);
+  for (size_t i = 0; i < z.size(); ++i) {
+    std::cin >> z[i];
   }
-  std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+  std::cout << (get_majority_element(z, 0, z.size(), n) != -1) << '\n';
 }
+
